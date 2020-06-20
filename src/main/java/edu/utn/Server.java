fe8fc6019d8cc4bd80edff5ac1f39cbe54818e0b
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-19 23:34:08 -0300

    First commit

---
+++ src/main/java/edu/utn/Server.java
@@ -0,0 +1,13 @@
+package edu.utn;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
