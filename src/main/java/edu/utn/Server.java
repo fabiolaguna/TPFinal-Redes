c0764e80ff326509e45db30ba45f0a1cd0fc1d3a
FedeAlesandro <fede.alesandro@gmail.com>
2020-06-19 23:42:10 -0300

    Primer commit

--- src/main/java/edu/utn/Server.java
+++ src/main/java/edu/utn/Server.java
@@ -1,13 +1,34 @@
 package edu.utn;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
-public class Server 
+public class Server
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ServerSocket server = null;
+        Socket sc = null;
+        final int HOST = 3000;
+        DataInputStream inputStream;
+        DataOutputStream outputStream;
+
+        try{
+            server = new ServerSocket(HOST);
+            System.out.println("Server initialized");
+
+            while (true){
+                sc = server.accept();
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
